54942dae2d8f883ce1d70106df22f9523892a84b
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-09-26 01:13:54 +0530

    Update App.js
    added terms route in the App.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { Route, Routes } from 'react-router-dom';
 
-import { Home, Subject, Leet, AboutPage, Contact } from './pages';
+import { Home, Subject, Leet, AboutPage, Contact, Terms } from './pages';
 import './App.scss';
 
 const App = () => {
@@ -13,6 +13,7 @@ const App = () => {
       <Route path='/Subject' element={<Subject />} />
       <Route path='/Leet' element={<Leet />} />
       <Route path='/Contact' element={<Contact />} />
+      <Route path='/Terms' element={<Terms />} />
     </Routes>
   );
 }
