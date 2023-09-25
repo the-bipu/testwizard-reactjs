589a4f2a5f860f2f16d2fb3e90733ce3099e9602
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-09-25 11:47:35 +0530

    Added Assets
    added images to the assets folder, although we don't need it locally cause we are going to render it from the sanity CMS.

---
+++ frontend-react/src/constants/images.js
@@ -0,0 +1,9 @@
+import aditya from '../assets/aditya-kumar.jpg';
+import slide from '../assets/home-slide-2.jpg';
+import logo from '../assets/logo_testwizard.png';
+
+export default {
+  aditya,
+  slide,
+  logo,
+};
