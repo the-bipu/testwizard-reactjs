80552f5010fa8ead4c595329d8e6ecae396590ea
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-09-26 01:18:11 +0530

    Update Leet Page
    Added Data directly from the Sanity to the site, also deleted extra codes. Added new button in the Syllabus section so that we can reduce the section count. Changes the styles of box.

--- frontend-react/src/pages/Leet/Leet.jsx
+++ frontend-react/src/pages/Leet/Leet.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './Leet.scss';
 
 import { Footer } from '../../container';
 import { Navbar } from '../../components';
 
+import { urlFor, client } from '../../client';
+
 const Leet = () => {
+
+    const [leet, setLeet] = useState([]);
+    const [leetNotes, setLeetNotes] = useState([])
+
+    useEffect(() => {
+        const query = '*[_type == "leet"]';
+    
+        client.fetch(query)
+          .then((data) => setLeet(data))
+      }, [])
+
+    useEffect(() => {
+        const query = '*[_type == "leetnotes"]';
+    
+        client.fetch(query)
+          .then((data) => setLeetNotes(data))
+      }, [])
+
   return (
     <div>
        <Navbar />
@@ -17,227 +37,48 @@ const Leet = () => {
 
         <section class="subjects">
 
-            <h1 class="heading">Syllabus</h1>
+            <h1 class="heading">Syllabus + Notes</h1>
 
             <div class="box-container">
 
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Chemistry.jpg" alt="" />
-                    <h3>Physics & Chemistry</h3>
-                    <p>Syllabus</p>
+                {leetNotes.map((leetNotes, index) => (
+                    <div class="box" key={leetNotes.name + index}>
+                        <img src={urlFor(leetNotes.imgUrl)} alt={leetNotes.name} />
+                        <h3>{leetNotes.name}</h3>
+                        <p>{leetNotes.description}</p>
+                        <a href={leetNotes.moduleLink}>
+                            <button>Syllabus</button>
+                        </a>
+                        &nbsp;&nbsp;
+                        <a href={leetNotes.moduleLink}>
+                            <button>Notes</button>
+                        </a>
                     </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Mathematics.jpg" alt="" />
-                    <h3>Mathematics</h3>
-                    <p>Syllabus</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Group-A.jpg" alt="" />
-                    <h3>Group-A</h3>
-                    <p>Syllabus</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Group-B.jpg" alt="" />
-                    <h3>Group-B</h3>
-                    <p>Syllabus</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Group-C.jpg" alt="" />
-                    <h3>Group-C</h3>
-                    <p>Syllabus</p>
-                    </div>
-                </a>
+                ))}
 
             </div>
             <br /><br />
 
-            <h1 class="heading">Physics, Chemistry and Mathematics</h1>
+            <h1 class="heading">Test Papers</h1>
 
             {/* 20 mathematics, 15 physics and 15 chemistry
             50 minutes for 50 questions */}
 
             <div class="box-container">
 
-                <div class="box">
-                    <img src="images/leet 2012.jpg" alt="" />
-                    <h3>LEET PCM</h3>
-                    <p>Year 2012</p>
-                    <a href="./LEET-Sets/01_SET_PCM_2012.html">
-                        <button class="test_button">Take Test</button>
-                    </a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2013.jpg" alt="" />
-                    <h3>LEET PCM</h3>
-                    <p>Year 2013</p>
-                    <a href="./LEET-Sets/02_SET_PCM_2013.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2014.jpg" alt="" />
-                    <h3>LEET PCM</h3>
-                    <p>Year 2014</p>
-                    <a href="./LEET-Sets/03_SET_PCM_2014.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2015.jpg" alt="" />
-                    <h3>LEET PCM</h3>
-                    <p>Year 2015</p>
-                    <a href="./LEET-Sets/04_SET_PCM_2015.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
+                {leet.map((leet, index) => (
+                        <div class="box" key={leet.name + index}>
+                            <img src={urlFor(leet.imgUrl)} alt={leet.name} />
+                            <h3>{leet.name}</h3>
+                            <p>{leet.year}</p>
+                            <a href={leet.moduleLink}>
+                                <button>Take Test</button>
+                            </a>
+                        </div>
+                ))}
 
             </div>
             <br /><br />
-
-            <h1 class="heading">Group A (Electric, Electronic and Computer Group)</h1>
-
-            <div class="box-container">
-
-                <div class="box">
-                    <img src="images/leet 2012.jpg" alt="" />
-                    <h3>LEET Group-A</h3>
-                    <p>Year 2012</p>
-                    <a href="./LEET-Sets/01_SET_GROUP_A_2012.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2013.jpg" alt="" />
-                    <h3>LEET Group-A</h3>
-                    <p>Year 2013</p>
-                    <a href="./LEET-Sets/02_SET_GROUP_A_2013.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2014.jpg" alt="" />
-                    <h3>LEET Group-A</h3>
-                    <p>Year 2014</p>
-                    <a href="./LEET-Sets/03_SET_GROUP_A_2014.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2015.jpg" alt="" />
-                    <h3>LEET Group-A</h3>
-                    <p>Year 2015</p>
-                    <a href="./LEET-Sets/04_SET_GROUP_A_2015.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-            </div>
-            <br /><br />
-
-            <h1 class="heading">General Knowledge, Mental Aptitude & English</h1>
-
-            <div class="box-container">
-
-                <div class="box">
-                    <img src="images/leet 2012.jpg" alt="" />
-                    <h3>LEET Aptitude</h3>
-                    <p>Year 2012</p>
-                    <a href="./LEET-Sets/01_SET_APTITUDE_2012.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2013.jpg" alt="" />
-                    <h3>LEET Aptitude</h3>
-                    <p>Year 2013</p>
-                    <a href="./LEET-Sets/02_SET_APTITUDE_2013.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2014.jpg" alt="" />
-                    <h3>LEET Aptitude</h3>
-                    <p>Year 2014</p>
-                    <a href="./LEET-Sets/03_SET_APTITUDE_2014.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-                <div class="box">
-                    <img src="images/leet 2015.jpg" alt="" />
-                    <h3>LEET Aptitude</h3>
-                    <p>Year 2015</p>
-                    <a href="./LEET-Sets/04_SET_APTITUDE_2015.html">
-                    <button class="test_button">Take Test</button></a>
-                </div>
-
-            </div>
-            <br /><br />
-
-            <h1 class="heading">Notes Section</h1>
-
-            <div class="box-container">
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Physics.jpg" alt="" />
-                    <h3>Physics</h3>
-                    <p>Notes of Physics</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Chemistry.jpg" alt="" />
-                    <h3>Chemistry</h3>
-                    <p>Notes of Physics</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Mathematics.jpg" alt="" />
-                    <h3>Mathematics</h3>
-                    <p>Notes of Physics</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Group-A.jpg" alt="" />
-                    <h3>Group-A</h3>
-                    <p>Notes of Group-A</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Group-B.jpg" alt="" />
-                    <h3>Group-B</h3>
-                    <p>Notes of Group-B</p>
-                    </div>
-                </a>
-
-                <a href="" target="_blank" rel="noopener noreferrer">
-                    <div class="box">
-                    <img src="images/Group-C.jpg" alt="" />
-                    <h3>Group-C</h3>
-                    <p>Notes of Group-C</p>
-                    </div>
-                </a>
-
-            </div>
 
         </section>
 
