dc6ee7876dd878a62f1b2401cb785e22e9c4deda
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-09-25 11:49:43 +0530

    Update About Component
    Here i have modified the code of About Component, deleted extra links and also designed the buttons.

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -5,99 +5,99 @@ import './About.scss';
 const About = () => {
   return (
     <div>
-      <section class="subjects">
+      <section className="subjects">
 
-        <h1 class="heading">our popular subjects</h1>
+        <h1 className="heading">our popular subjects</h1>
 
-        <div class="box-container">
+        <div className="box-container">
 
-          <div class="box">
-              <img src="images/subject-icon (1).jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>computer <br /> fundamentals</h3>
               <p>4 modules</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/subject-icon (2).jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>operating <br /> system</h3>
               <p>4 modules</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/subject-icon (3).jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>internet and web technology</h3>
               <p>4 modules</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/subject-icon (4).jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>computer <br /> programming</h3>
               <p>4 modules</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/subject-icon (5).jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>data <br /> structure</h3>
               <p>4 modules</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
         </div>
         <br />
 
-        <div class="view">
-          <a href="subject.php" class="view-more-sub"><button>View More</button></a>
+        <div className="view">
+          <a href="subject.php" className="view-more-sub"><button>View More</button></a>
         </div>
         <br /><br />
 
-        <h1 class="heading">our popular sets</h1>
+        <h1 className="heading">our popular sets</h1>
 
-        <div class="box-container">
+        <div className="box-container">
 
-          <div class="box">
-              <img src="images/leet 2012.jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>LEET Group-A</h3>
               <p>Year 2012</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/leet 2013.jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>LEET Group-A</h3>
               <p>Year 2013</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/leet 2014.jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>LEET Group-A</h3>
               <p>Year 2014</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/leet 2015.jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>LEET Group-A</h3>
               <p>Year 2015</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
-          <div class="box">
-              <img src="images/leet 2016.jpg" alt="" />
+          <div className="box">
+              <img src="" alt="" />
               <h3>LEET Group-A</h3>
               <p>Year 2016</p>
-                <a href="leet.php"><button>Visit</button></a>
+                <a href=""><button>Visit</button></a>
           </div>
 
         </div>
         <br />
 
-        <div class="view">
-          <a href="leet.php" class="view-more-sub"><button>View More</button></a>
+        <div className="view">
+          <a href="" className="view-more-sub"><button>View More</button></a>
         </div>
 
         </section>
