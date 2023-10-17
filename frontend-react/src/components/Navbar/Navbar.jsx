afef8f47cb42130081654f91b42fe0dc2178c611
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-17 21:36:39 +0530

    Update Navbar.jsx

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import './Navbar.scss';
 
 const Navbar = () => {
+
+  const [isActive, setIsActive] = useState(false);
+  
+  const toggleNavbar = () => {
+    setIsActive(!isActive);
+  };
+
   return (
-      <div className="header">
+      // <div className="header">
 
-        <a href="#" className="logo"> <i className="fas fa-lightbulb"></i> TestWizard </a>
+      //   <a href="#" className="logo"> <i className="fas fa-lightbulb"></i> TestWizard </a>
 
-          <nav className="navbar">
-            <div id="close-navbar" className="fas fa-times"></div>
-              <Link to="/">Home</Link>
-              <Link to="/Subject">Subject</Link>
-              <Link to="/Leet">Leet</Link>
-              <Link to="/AboutPage">About</Link>
-              <Link to="/Contact">Contact</Link>
-          </nav>
+      //     <nav className="navbar">
+      //       <div id="close-navbar" className="fas fa-times"></div>
+      //         <Link to="/">Home</Link>
+      //         <Link to="/Subject">Subject</Link>
+      //         <Link to="/Leet">Leet</Link>
+      //         <Link to="/AboutPage">About</Link>
+      //         <Link to="/Contact">Contact</Link>
+      //     </nav>
+      // </div>
+
+      <div className={`navbar--main ${isActive ? 'active' : ''}`}>
+      <div className='container'>
+            <a href="#" className="logo"> <i className="fas fa-lightbulb"></i> TestWizard </a>
+
+            <div id="menu-btn" onClick={toggleNavbar} className="fas fa-bars"></div>
+
+            <nav className="navbar">
+              <div id="close-navbar" className="fas fa-times" onClick={toggleNavbar}></div>
+                <Link to="/">Home</Link>
+                <Link to="/Subject">Subject</Link>
+                <Link to="/Leet">Leet</Link>
+                <Link to="/AboutPage">About</Link>
+                <Link to="/Contact">Contact</Link>
+            </nav>
+
       </div>
+
+      <div id="sidebar" className={`sidebar ${isActive ? 'active' : ''}`}>
+        <div id="close-sidebar" className="fas fa-times" onClick={toggleNavbar}></div>
+
+        <Link to="/">Home</Link>
+        <Link to="/Subject">Subject</Link>
+        <Link to="/Leet">Leet</Link>
+        <Link to="/AboutPage">About</Link>
+        <Link to="/Contact">Contact</Link>
+      </div>
+
+    </div>
   )
 }
 
