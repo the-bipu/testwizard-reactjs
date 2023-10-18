19429c0b5bf75d054604b1e03760c7eb74592bd5
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-18 10:54:21 +0530

    Create Hero.jsx

---
+++ frontend-react/src/container/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Hero.scss';
+
+const Hero = () => {
+  return (
+    <div>
+        <section className="main--container-hero">
+            <div className="heading">Why this Site ??</div>
+        </section>
+    </div>
+  )
+}
+
+export default Hero
