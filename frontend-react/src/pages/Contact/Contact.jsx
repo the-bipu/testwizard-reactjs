10c261e96757bf5816289a388593b5e912fcf33a
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-18 10:54:07 +0530

    Update Contact.jsx

--- frontend-react/src/pages/Contact/Contact.jsx
+++ frontend-react/src/pages/Contact/Contact.jsx
@@ -10,33 +10,33 @@ const Contact = () => {
       <div>
         <Navbar />
 
-        <section class="heading-link">
+        <section className="heading-link">
             <h3>contact us</h3>
             <p> <a href="index">home</a> / contact </p>
         </section>
 
-        <section class="contact">
+        <section className="contact">
 
-            <h1 class="heading"> get in touch </h1>
+            <h1 className="heading"> get in touch </h1>
 
-            <div class="icons-container">
+            <div className="icons-container">
 
-                <div class="icons">
-                    <i class="fas fa-phone"></i>
+                <div className="icons">
+                    <i className="fas fa-phone"></i>
                     <h3>phone :</h3>
                     <p>+91 99999 99999</p>
                     <p>+91 88888 88888</p>
                 </div>
 
-                <div class="icons">
-                    <i class="fas fa-envelope"></i>
+                <div className="icons">
+                    <i className="fas fa-envelope"></i>
                     <h3> email : </h3>
                     <p>testwizardsliet@gmail.com</p>
                     <p>email@gmail.com</p>
                 </div>
 
-                <div class="icons">
-                    <i class="fas fa-map"></i>
+                <div className="icons">
+                    <i className="fas fa-map"></i>
                     <h3>address :</h3>
                     <p>India</p>
                 </div>
