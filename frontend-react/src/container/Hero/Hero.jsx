5d1a4f434d89c153716c8cc101c55692d0db7de0
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-19 22:33:19 +0530

    Update Hero.jsx

--- frontend-react/src/container/Hero/Hero.jsx
+++ frontend-react/src/container/Hero/Hero.jsx
@@ -7,6 +7,11 @@ const Hero = () => {
     <div>
         <section className="main--container-hero">
             <div className="heading">Why this Site ??</div>
+
+            <div className="main--content-hero">
+              <div className="content--div-hero">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, labore! Cum, vel voluptatibus! Quisquam dicta sed minus, dolorum incidunt iste voluptatum nemo doloribus, maxime corporis perferendis cupiditate tempore, molestias nesciunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex quibusdam soluta unde at eius maxime, libero officiis, pariatur veniam odio natus provident, totam id aliquam molestiae illo sint excepturi! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis amet voluptate possimus deserunt alias nisi accusantium quibusdam. Omnis, id necessitatibus doloremque molestias, dolorem consequuntur, quo beatae delectus corrupti ea nesciunt!</div>
+              <div className="image--div-hero">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, labore! Cum, vel voluptatibus! Quisquam dicta sed minus, dolorum incidunt iste voluptatum nemo doloribus, maxime corporis perferendis cupiditate tempore, molestias nesciunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex quibusdam soluta unde at eius maxime, libero officiis, pariatur veniam odio natus provident, totam id aliquam molestiae illo sint excepturi! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis amet voluptate possimus deserunt alias nisi accusantium quibusdam. Omnis, id necessitatibus doloremque molestias, dolorem consequuntur, quo beatae delectus corrupti ea nesciunt!</div>
+            </div>
         </section>
     </div>
   )
