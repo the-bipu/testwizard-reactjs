afdc53e4a4a44dd2d5d2522ae4f2a2ed363b9b2e
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-26 22:33:40 +0530

    Update Hero.jsx

--- frontend-react/src/container/Hero/Hero.jsx
+++ frontend-react/src/container/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Hero.scss';
+import images from '../../constants/images';
 
 const Hero = () => {
   return (
@@ -10,7 +11,9 @@ const Hero = () => {
 
             <div className="main--content-hero">
               <div className="content--div-hero">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, labore! Cum, vel voluptatibus! Quisquam dicta sed minus, dolorum incidunt iste voluptatum nemo doloribus, maxime corporis perferendis cupiditate tempore, molestias nesciunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex quibusdam soluta unde at eius maxime, libero officiis, pariatur veniam odio natus provident, totam id aliquam molestiae illo sint excepturi! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis amet voluptate possimus deserunt alias nisi accusantium quibusdam. Omnis, id necessitatibus doloremque molestias, dolorem consequuntur, quo beatae delectus corrupti ea nesciunt!</div>
-              <div className="image--div-hero">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex, labore! Cum, vel voluptatibus! Quisquam dicta sed minus, dolorum incidunt iste voluptatum nemo doloribus, maxime corporis perferendis cupiditate tempore, molestias nesciunt! Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ex quibusdam soluta unde at eius maxime, libero officiis, pariatur veniam odio natus provident, totam id aliquam molestiae illo sint excepturi! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis amet voluptate possimus deserunt alias nisi accusantium quibusdam. Omnis, id necessitatibus doloremque molestias, dolorem consequuntur, quo beatae delectus corrupti ea nesciunt!</div>
+              <div className="image--div-hero">
+                <img className='image--group' src={images.group} alt="" />
+              </div>
             </div>
         </section>
     </div>
