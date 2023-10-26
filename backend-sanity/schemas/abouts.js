ac98f5950784c8f32bd03b3c0030c0b7c3e8a988
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-27 00:23:09 +0530

    Update abouts.js

--- backend-sanity/schemas/abouts.js
+++ backend-sanity/schemas/abouts.js
@@ -9,6 +9,11 @@ export default{
             type:'string'
         },
         {
+            name:'title',
+            title:'Title',
+            type:'string'
+        },
+        {
             name:'description',
             title:'Description',
             type:'string'
