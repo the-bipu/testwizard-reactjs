1ba3b625fa08e27a7403325c4ac748b36272727a
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-27 00:23:13 +0530

    Update AboutPage.jsx

--- frontend-react/src/pages/AboutPage/AboutPage.jsx
+++ frontend-react/src/pages/AboutPage/AboutPage.jsx
@@ -56,23 +56,27 @@ const AboutPage = () => {
             <section className="about-section">
 
                 <h1 className="heading">our team</h1>
+
                 <section className="us-section">
 
                     {abouts.map((abouts, index) => (
                         <div className="box-us-section" key={abouts.name + index}>
                             <img src={urlFor(abouts.imgUrl)} alt={abouts.name} />
                             <h3>{abouts.name}</h3>
-                            <p>{abouts.description}</p>
+                            <p>{abouts.title}</p>
                             <div className="share">
                                 <a href="#" className="fab fa-facebook-f"></a>
                                 <a href="#" className="fab fa-twitter"></a>
                                 <a href="#" className="fab fa-instagram"></a>
                                 <a href="#" className="fab fa-linkedin"></a>
                             </div><br />
-                            <button className="box-button"><a href="more-about.php">More</a></button>
                         </div>
                     ))}
 
+                </section>
+
+                <section className="button--section">
+                    <button className="box-button"><a href="">Show Detailed Version !!</a></button>
                 </section>
 
                 <section className="us-section--large">
@@ -85,13 +89,14 @@ const AboutPage = () => {
                             <div className="content--div">
                                 <h3>{abouts.name}</h3>
                                 <p>{abouts.description}</p>
+
+                                <br />
                                 <div className="share">
                                     <a href="#" className="fab fa-facebook-f"></a>
                                     <a href="#" className="fab fa-twitter"></a>
                                     <a href="#" className="fab fa-instagram"></a>
                                     <a href="#" className="fab fa-linkedin"></a>
-                                </div><br />
-                                <button className="box-button"><a href="more-about.php">More</a></button>
+                                </div>
                             </div>
                         </div>
                     ))}
@@ -110,43 +115,3 @@ const AboutPage = () => {
 }
 
 export default AboutPage;
-
-{/* <div class="box-us-section">
-                <img src="images/manas-madhukar.jpg" alt="">
-                <h3>Manas Madhukar</h3>
-                <p class="skill">Front-end Developer</p>
-                <p class="about-me">Hello and welcome to my about page! I am a front-end developer who is passionate about creating visually stunning and engaging web experiences. From a young age, I have been drawn to the world of design and technology, and I have found my calling in the field of web development. As a front-end developer, I specialize in creating the user-facing side of websites, using HTML, CSS, and JavaScript to bring designs to life. I have experience working on a variety of projects, from small business websites to complex web applications. When I'm not coding, you can find me exploring new coffee shops, hiking in the mountains, or playing video games. I am excited to continue growing my skills and collaborating with others in the industry. Thanks for stopping by!</p>
-                <div class="share">
-                    <a href="#" class="fab fa-facebook-f"></a>
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-instagram"></a>
-                    <a href="#" class="fab fa-linkedin"></a>
-                </div>
-                <!-- <button class="box-button">More</button> -->
-            </div>
-
-            <div class="box-us-section">
-                <img src="images/aditya-kumar.jpg" alt="">
-                <h3>Aditya Kumar</h3>
-                <p class="skill">Full-Stack Web Developer</p>
-                <p class="about-me">Hello and welcome to my about page! I am a full-stack developer with a passion for creating dynamic and interactive web applications. My expertise spans both front-end and back-end development, allowing me to design and implement end-to-end solutions that meet the needs of users and businesses alike. With a strong foundation in programming languages such as JavaScript, Python, and Java, I am experienced in building responsive user interfaces, integrating databases, and creating server-side logic. I enjoy collaborating with other developers, designers, and stakeholders to bring projects to life. When I'm not coding, you can find me playing guitar, practicing yoga, or exploring the great outdoors. I am excited about the endless possibilities of full-stack development and the potential for innovation it brings. Thanks for stopping by my page!</p>
-                <div class="share">
-                    <a href="#" class="fab fa-facebook-f"></a>
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-instagram"></a>
-                    <a href="#" class="fab fa-linkedin"></a>
-                </div>
-            </div>
-
-            <div class="box-us-section">
-                <img src="images/bipanshu-kumar.jpg" alt="">
-                <h3>Bipanshu Kumar</h3>
-                <p class="skill">Backend Developer</p>
-                <p class="about-me">Hello and welcome to my about page! I am a back-end developer who is passionate about building robust and scalable web applications. With a strong background in programming languages such as Python, Java, and PHP, I specialize in developing the server-side of websites, creating APIs, and integrating databases. I enjoy working on complex projects that require creative problem-solving and attention to detail. My experience in the industry has allowed me to collaborate with other developers, designers, and stakeholders to deliver high-quality software solutions. When I'm not coding, I enjoy playing basketball, reading science fiction novels, and exploring new cities. I am excited about the future of web development and the opportunities it brings for innovation and creativity. Thanks for stopping by my page!</p>
-                <div class="share">
-                    <a href="#" class="fab fa-facebook-f"></a>
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-instagram"></a>
-                    <a href="#" class="fab fa-linkedin"></a>
-                </div>
-            </div> */}
