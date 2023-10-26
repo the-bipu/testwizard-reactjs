f139403bb5837f0b658eb44effed02a8a0138421
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-26 22:33:35 +0530

    Added new asset

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -2,10 +2,12 @@ import aditya from '../assets/aditya-kumar.jpg';
 import slide from '../assets/home-slide-2.jpg';
 import logo from '../assets/logo_testwizard.png';
 import eclipse from '../assets/eclipse.png';
+import group from '../assets/group-illust.jpg';
 
 export default {
   aditya,
   slide,
   logo,
   eclipse,
+  group
 };
