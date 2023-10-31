cf67685b1b4689982e2b3525232788b74947d290
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-31 23:07:08 +0530

    Update index.js

--- frontend-react/src/pages/index.js
+++ frontend-react/src/pages/index.js
@@ -3,5 +3,7 @@ export { default as AboutPage } from './AboutPage/AboutPage';
 export { default as Subject } from './Subject/Subject';
 export { default as Leet } from './Leet/Leet';
 export { default as Contact } from './Contact/Contact';
+export { default as Login } from './Login/Login';
+export { default as Register } from './Register/Register';
 export { default as Terms } from './Extra/Terms/Terms';
 export { default as SubjectTest } from './SubjectTest/SubjectTest';
