83fffbb0ab41595d311bd3ab30accb19b58d1c8e
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-10-31 23:07:11 +0530

    Update App.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Home, Subject, Leet, AboutPage, Contact, Terms, SubjectTest } from './pages';
+import { Home, Subject, Leet, AboutPage, Contact, Login, Register, Terms, SubjectTest } from './pages';
 import './App.scss';
 
 const App = () => {
@@ -12,6 +12,8 @@ const App = () => {
       <Route path='/Subject' element={<Subject />} />
       <Route path='/Leet' element={<Leet />} />
       <Route path='/Contact' element={<Contact />} />
+      <Route path='/Login' element={<Login />} />
+      <Route path='/Register' element={<Register />} />
       <Route path='/Terms' element={<Terms />} />
 
       <Route path='/subjectTest/:subjectName/:subCode' element={<SubjectTest />} />
