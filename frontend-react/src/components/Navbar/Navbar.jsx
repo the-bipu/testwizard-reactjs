5fd64d7ce62bbf49e2bad528bd6786f0b19c41a3
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-01 20:46:00 +0530

    Update Navbar.jsx

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -12,19 +12,6 @@ const Navbar = () => {
   };
 
   return (
-      // <div className="header">
-
-      //   <a href="#" className="logo"> <i className="fas fa-lightbulb"></i> TestWizard </a>
-
-      //     <nav className="navbar">
-      //       <div id="close-navbar" className="fas fa-times"></div>
-      //         <Link to="/">Home</Link>
-      //         <Link to="/Subject">Subject</Link>
-      //         <Link to="/Leet">Leet</Link>
-      //         <Link to="/AboutPage">About</Link>
-      //         <Link to="/Contact">Contact</Link>
-      //     </nav>
-      // </div>
 
       <div className={`navbar--main ${isActive ? 'active' : ''}`}>
       <div className='container'>
