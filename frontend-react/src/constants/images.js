79a4378d8f8e816c7ec881ed00fb327442d78e59
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-01 20:45:55 +0530

    Added new assets

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -3,11 +3,17 @@ import slide from '../assets/home-slide-2.jpg';
 import logo from '../assets/logo_testwizard.png';
 import eclipse from '../assets/eclipse.png';
 import group from '../assets/group-illust.jpg';
+import bb8 from '../assets/bb-8.jpg';
+import grogu from '../assets/grogu.jpg';
+import login from '../assets/login.jpg';
 
 export default {
   aditya,
   slide,
   logo,
   eclipse,
-  group
+  group,
+  grogu,
+  bb8,
+  login,
 };
