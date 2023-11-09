a58252385804034a23773fd7f984bf7f9e52d2be
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-10 01:14:07 +0530

    Update SubjectDetail.jsx

--- frontend-react/src/pages/SubjectDetail/SubjectDetail.jsx
+++ frontend-react/src/pages/SubjectDetail/SubjectDetail.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { client, urlFor } from '../../client';
+
+import { Navbar } from '../../components';
+import './SubjectDetail.scss';
 
 const SubjectDetail = () => {
   const { subjectName, subCode } = useParams();
@@ -18,11 +21,34 @@ const SubjectDetail = () => {
   }
 
   return (
-    <div>
-      <h1>{subject.name}</h1>
-      <img src={urlFor(subject.imgUrl)} alt={subject.name} />
-      <p>{subject.subCode}</p>
-      <p>{subject.modules}</p>
+    <div className='main--div'>
+        <Navbar />
+
+        <section className='detail--div'>
+            <img src={urlFor(subject.imgUrl)} alt={subject.name} />
+            <h1 className='heading--sub'>{subject.name}</h1>
+            <p className='para--sub'>CODE: {subject.subCode}</p>
+            <p className='para--sub'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eius expedita quod ex eos accusamus saepe nobis. Placeat minima dolorum illum quae perspiciatis quidem culpa, cupiditate laborum maxime beatae non! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis distinctio quaerat quos ipsum quod nemo. Nesciunt ratione quia excepturi esse ipsa minus optio quas, corporis provident error soluta! Amet, at?</p>
+
+            <div className="buttons--div">
+                <Link to={{
+                    pathname: `/subjectTest/${subject.name}/${subject.subCode}`
+                }}>
+                        <button className='test_button'>Take Test</button>
+                </Link>
+
+                <Link to={{
+                    pathname: ``
+                }}>
+                        <button className='test_button'>Notes</button>
+                </Link>
+            </div>
+
+        </section>
+
+            <div className="footer--sub">
+                <p>COPYRIGHT @2023 / TESTWIZARD</p>
+            </div>
     </div>
   );
 }
