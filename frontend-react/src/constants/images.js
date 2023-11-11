03c48c64a3cd6cf3ad6058f095c126bc543b8fd0
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-11 14:15:06 +0530

    Update images.js

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -5,7 +5,7 @@ import eclipse from '../assets/eclipse.png';
 import group from '../assets/group-illust.jpg';
 import bb8 from '../assets/bb-8.jpg';
 import grogu from '../assets/grogu.jpg';
-import login from '../assets/login.jpg';
+import login from '../assets/login.png';
 
 // eslint-disable-next-line
 export default {
