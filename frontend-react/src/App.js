d2e6b5df2f66a814e61a7b8dbdb57f19bc43e989
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-20 12:44:41 +0530

    Update App.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Home, Subject, Leet, AboutPage, Contact, Login, Terms, SubjectTest, SubjectDetail } from './pages';
+import { Home, Subject, Leet, AboutPage, Contact, Login, Terms, SubjectTest, SubjectDetail, Dashboard } from './pages';
 import './App.scss';
+import { UserProvider } from './context/userContext';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/AboutPage' element={<AboutPage />} />
-      <Route path='/Subject' element={<Subject />} />
-      <Route path='/Leet' element={<Leet />} />
-      <Route path='/Contact' element={<Contact />} />
-      <Route path='/Login' element={<Login />} />
-      <Route path='/Terms' element={<Terms />} />
+    <UserProvider>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/AboutPage' element={<AboutPage />} />
+        <Route path='/Subject' element={<Subject />} />
+        <Route path='/Leet' element={<Leet />} />
+        <Route path='/Contact' element={<Contact />} />
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Terms' element={<Terms />} />
+        <Route path='/Dashboard' element={<Dashboard />} />
 
-      <Route path='/subjectTest/:subjectName/:subCode' element={<SubjectTest />} />
-      <Route path="/subjectDetail/:subjectName/:subCode" element={<SubjectDetail />} />
-    </Routes>
+        <Route path='/subjectTest/:subjectName/:subCode' element={<SubjectTest />} />
+        <Route path="/subjectDetail/:subjectName/:subCode" element={<SubjectDetail />} />
+      </Routes>
+    </UserProvider>
   );
 }
 
