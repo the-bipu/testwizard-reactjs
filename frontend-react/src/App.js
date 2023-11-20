457632d77bb2caa315d565045401949d3cf3dac1
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-20 23:17:25 +0530

    Added adminDashboard

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import { Home, Subject, Leet, AboutPage, Contact, Login, Terms, SubjectTest, SubjectDetail, Dashboard } from './pages';
+import { Home, Subject, Leet, AboutPage, Contact, Login, Terms, SubjectTest, SubjectDetail, Dashboard, AdminDashboard } from './pages';
 import './App.scss';
 import { UserProvider } from './context/userContext';
 
@@ -17,6 +17,7 @@ const App = () => {
         <Route path='/Login' element={<Login />} />
         <Route path='/Terms' element={<Terms />} />
         <Route path='/Dashboard' element={<Dashboard />} />
+        <Route path='/AdminDashboard' element={<AdminDashboard />} />
 
         <Route path='/subjectTest/:subjectName/:subCode' element={<SubjectTest />} />
         <Route path="/subjectDetail/:subjectName/:subCode" element={<SubjectDetail />} />
