d854899d7c70c6699e26382bf34d5f29a4b4cd43
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-04 23:41:40 +0530

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -33,33 +33,25 @@ router.post('/login', async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        // As admin
-        const adminUsername = "bipanshu";
-        const adminPassword = "bipanshu123";
-
-        if (username === adminUsername && password === adminPassword) {
-            // You're logging in as an admin
-            return res.status(200).json({ message: 'Login successful as Admin.', isAdmin: true });
-        }
-
         // Find the user by username
         const user = await User.findOne({ username });
 
-        const token = await user.generateAuthToken();
-
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
+        } else {
+            // Check if the entered password matches the stored hashed password
+            const isPasswordValid = await user.comparePassword(password);
+
+            if (!isPasswordValid) {
+                return res.status(401).json({ message: 'Invalid password' });
+            } else {
+                // Generate and retrieve the authentication token
+                const token = await user.generateAuthToken();
+                
+                // Return success response with token
+                res.status(200).json({ message: 'Login successful as User.', isAdmin: user.isAdmin, token });
+            }
         }
-
-        // Check if the provided password matches the stored password
-        const isPasswordValid = await user.comparePassword(password);
-
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Invalid password' });
-        }
-
-        res.status(200).json({ message: 'Login successful as User.', isAdmin: false });
-
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
