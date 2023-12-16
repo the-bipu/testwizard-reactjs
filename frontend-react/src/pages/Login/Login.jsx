88f24974c8c96556deec2bce3f85225a2ae95f98
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-16 22:08:46 +0530

    Update Login.jsx

--- frontend-react/src/pages/Login/Login.jsx
+++ frontend-react/src/pages/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 
   const [isLogin, setIsLogin] = useState(true);
   const [loginData, setLoginData] = useState({ username: '', password: '' });
-  const [registerData, setRegisterData] = useState({ email: '', username: '', password: '' });
+  const [registerData, setRegisterData] = useState({ email: '', username: '', password: '', age: '', gender: '', phone: '', fname: '', lname: '' });
 
   // Retrieve data from localStorage on component mount
   useEffect(() => {
@@ -87,18 +87,18 @@ const Login = () => {
         setLoggedIn(true);
         setLoggedInUsername(isLogin ? loginData.username : registerData.username);
 
-        window.location.href = '/';
+        window.location.href = '/Dashboard';
         console.log('Success');
 
         isLogin
           ? setLoginData({ username: '', password: '' })
-          : setRegisterData({ email: '', username: '', password: '' });
+          : setRegisterData({ email: '', username: '', password: '', age: '', gender: '', phone: '', fname: '', lname: '' });
       } else {
         window.alert(`Operation failed: ${result.message}`);
         console.error('Failed');
         isLogin
           ? setLoginData({ username: '', password: '' })
-          : setRegisterData({ email: '', username: '', password: '' });
+          : setRegisterData({ email: '', username: '', password: '', age: '', gender: '', phone: '', fname: '', lname: '' });
       }
     } catch (error) {
       console.error('Error:', error);
@@ -121,7 +121,7 @@ const Login = () => {
 
           <div className="content__div">
 
-            <form action='' onSubmit={handleSubmit}>
+            <form action='/' onSubmit={handleSubmit}>
 
               <div className="login__div" style={{ display: isLogin ? 'block' : 'none' }}>
 
@@ -159,6 +159,7 @@ const Login = () => {
 
                 <div className="main--contentDiv">
                   <div className="first--section">
+
                     <div className="email__div">
                       <label htmlFor="Email" className='label__input'>Email</label>
                       <input type="email" name="email" onChange={handleInputChange} pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" title="Must be a valid Email" className="email__input input__box" required={!isLogin} />
@@ -167,30 +168,35 @@ const Login = () => {
                     <div className="names--div">
                       <div className="username__div">
                         <label htmlFor="Name" className='label__input'>First Name</label>
-                        <input type="text" name="fname" onChange={handleInputChange} pattern="[a-zA-Z]" className="username__input input__box" required={!isLogin} />
+                        <input type="text" name="fname" onChange={handleInputChange} className="username__input input__box" required={!isLogin} />
                       </div>
 
                       <div className="username__div">
                         <label htmlFor="Name" className='label__input'>Second Name</label>
-                        <input type="text" name="sname" onChange={handleInputChange} pattern="[a-zA-Z]" className="username__input input__box" required={!isLogin} />
+                        <input type="text" name="lname" onChange={handleInputChange} className="username__input input__box" required={!isLogin} />
                       </div>
                     </div>
 
                     <div className="age--type">
                       <div className="age--div">
                         <label htmlFor="Name" className='label__input'>Age</label>
-                        <input type="number" name="fname" onChange={handleInputChange} pattern="[0-9]" className="username__input input__box" required={!isLogin} />
+                        <input type="number" name="age" onChange={handleInputChange} pattern="[0-9]" className="age__input input__box" required={!isLogin} />
                       </div>
 
                       <div className="gender--div">
                         <label htmlFor="Name" className='label__input'>Gender</label>
-                        <input type="text" name="fname" onChange={handleInputChange} pattern="[0-9]" className="username__input input__box" required={!isLogin} />
+                        <select type='text' name="gender" className='gender__input input__box' onChange={handleInputChange} required={!isLogin}>
+                          <option value="Male">------</option>
+                          <option value="Male">Male</option>
+                          <option value="Female">Female</option>
+                        </select>
+                        {/* <input type="text" name="fname" onChange={handleInputChange} pattern="[0-9]" className="username__input input__box" required={!isLogin} /> */}
                       </div>
                     </div>
 
                     <div className="username__div">
                       <label htmlFor="Phone" className='label__input'>Phone Number</label>
-                      <input type="number" name="phone" onChange={handleInputChange} pattern="[0-9._]" title="Must be a valid Number" className="email__input input__box" required={!isLogin} />
+                      <input type="number" name="phone" onChange={handleInputChange} pattern="[0-9]" title="Must be a valid Number" className="phone__input input__box" required={!isLogin} />
                     </div>
                   </div>
 
