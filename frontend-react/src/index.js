4bab762a8c76a2db54a02ab87feb704042025cfe
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-12-18 04:09:47 +0530

    code fixes

--- frontend-react/src/index.js
+++ frontend-react/src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom'
-import { Theme } from '@radix-ui/themes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Theme>
-      <App />
-    </Theme>
+    <App />
   </BrowserRouter>
 );
